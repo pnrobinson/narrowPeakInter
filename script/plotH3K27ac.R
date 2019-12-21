ad9d01a6356b5b541069d11e70318458b4f370f2
Peter Robinson <peter.robinson@jax.org>
2019-12-21 12:48:50 -0500

    Updating generation of figure

--- script/plotH3K27ac.R
+++ script/plotH3K27ac.R
@@ -14,7 +14,7 @@ g <- g + theme_classic()
 g <- g + scale_x_discrete(c("CGI", "Non-CGI"))
 #g <- g + scale_fill_brewer(palette = "Dark2")
 g <- g + scale_y_continuous(trans='log10')
-g <- g + stat_compare_means(label.x=1.3, label.y=4, size=6, legend = "none")
+g <- g + stat_compare_means(label.x=1.25, label.y=4, size=5, legend = "none")
 g <- g + theme(axis.title.x = element_blank(),
                axis.title.y = element_text(size=24),
                axis.text.x = element_text(size=28),
@@ -33,7 +33,7 @@ g <- g + theme_classic()
 g <- g + scale_x_discrete(c("CGI", "Non-CGI"))
 #g <- g + scale_fill_brewer(palette = "Dark2")
 g <- g + scale_y_continuous(trans='log10')
-g <- g + stat_compare_means(label.x=1.3, label.y=4, size=6, legend = "none")
+g <- g + stat_compare_means(label.x=1.25, label.y=4, size=5, legend = "none")
 g <- g + theme(axis.title.x = element_blank(),
                axis.title.y = element_text(size=24),
                axis.text.x = element_text(size=28),
